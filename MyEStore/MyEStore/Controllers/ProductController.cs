618ac6eb54e91cb77f75b5ac02cc079ec75646b3
Hien Luong Tran Hy <hyhien@gmail.com>
2025-11-07 07:46:51 +0700

    Init project + List product

---
+++ MyEStore/MyEStore/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEStore.Entities;
+using MyEStore.Models;
+
+namespace MyEStore.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly MyeStoreContext _ctx;
+
+		public ProductController(MyeStoreContext context)
+		{
+			_ctx = context;
+		}
+
+		public IActionResult Index()
+		{
+			var data = _ctx.HangHoas.AsQueryable();
+			var result = data.Select(hh => new HangHoaVM
+			{
+				MaHh = hh.MaHh,
+				TenHh = hh.TenHh,
+				DonGia = hh.DonGia ?? 0,
+				Hinh = hh.Hinh
+			}).ToList();
+			return View(result);
+
+		}
+	}
+}
