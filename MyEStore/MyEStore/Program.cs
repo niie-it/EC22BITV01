f1558b47de7203f9efd0df6ac7b575da210f628a
Hien Luong Tran Hy <hyhien@gmail.com>
2025-11-07 09:37:53 +0700

    Gio hang

--- MyEStore/MyEStore/Program.cs
+++ MyEStore/MyEStore/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Product}/{action=Index}/{id?}");
 
 app.Run();
