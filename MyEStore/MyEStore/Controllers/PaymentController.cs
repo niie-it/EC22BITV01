7ab1be58e01f06271e9de6a29d4ac5f9f9d7e9e0
Hien Luong Tran Hy <hyhien@gmail.com>
2025-11-21 14:29:45 +0700

    Paypal

---
+++ MyEStore/MyEStore/Controllers/PaymentController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEStore.Models;
+
+namespace MyEStore.Controllers
+{
+	public class PaymentController : Controller
+	{
+		private readonly PaypalClient _paypalClient;
+
+		public PaymentController(PaypalClient paypalClient)
+		{
+			_paypalClient = paypalClient;
+		}
+
+		[HttpPost("/payment/create-paypal-order")]
+		public async Task<IActionResult> CreateOrder([FromBody] Amount amount)
+		{
+			//số tiền có thể lấy từ Session hoặc dưới client gửi lên
+			//var value = "1234";
+			var value = amount.value;
+			//var currency_code = "USD";
+			var currency_code = amount.currency_code;
+
+			//Lưu đơn hàng vô database ==> refId chính là mã đơn hàng
+			var refId = DateTime.Now.Ticks.ToString();
+
+			try
+			{
+				var response = await _paypalClient.CreateOrder(value, currency_code, refId);
+				return Ok(response);
+			}
+			catch (Exception ex)
+			{
+				var error = new { ex.GetBaseException().Message };
+				return BadRequest(error);
+			}
+		}
+
+		[HttpPost("/payment/capture-paypal-order")]
+		public IActionResult CaptureOrder(string orderID)
+		{
+			try
+			{
+				var response = _paypalClient.CaptureOrder(orderID);
+
+
+				//Cập nhật trạng thái đơn hàng vô database
+
+
+				return Ok(response);
+			}
+			catch (Exception ex)
+			{
+				var error = new { ex.GetBaseException().Message };
+				return BadRequest(error);
+			}
+		}
+	}
+}
